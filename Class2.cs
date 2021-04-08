75619000737ab31bc9b31dc09d52c69a21c3874b
stryuuuk <73072779+stryuuuk@users.noreply.github.com>
2021-04-08 14:26:09 +0300

    constructor and 2 fields for node class

--- Class2.cs
+++ Class2.cs
@@ -6,5 +6,13 @@ namespace lab2
 {
     class Node
     {
+        public int Data;
+        public Node left, right;
+
+        public Node(int data)
+        {
+            Data = data;
+            left = right = null;
+        }
     }
 }
