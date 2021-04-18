2e9a235b06a6376a930b487b95248d06dc4577d8
stryuuuk <73072779+stryuuuk@users.noreply.github.com>
2021-04-18 22:26:28 +0300

    fields

---
+++ ArrayList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    class ArrayList<T> where T: IComparable
+    {
+        public int Max;        
+        public int Last;        
+        public T[] Array;
+        
+    }
+}
