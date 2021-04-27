b20ed448ae5566b5742d6b503ff57d784d1fae73
stryuuuk <73072779+stryuuuk@users.noreply.github.com>
2021-04-27 14:21:09 +0300

    some fields and constractor for Graph class

--- WeightedGraph.cs
+++ WeightedGraph.cs
@@ -6,6 +6,16 @@ namespace lab4
 {
     class WeightedGraph<T> where T:IComparable<T>
     {
-       
+        private List<Vertice<T>> Vertices;
+        private int[,] AdjMatrix;
+        private int Size;
+        private WeightedGraph(int size)
+        {
+
+        }
+        public void CreateWeightedGraph(int size)
+        {
+            WeightedGraph<T> graph = new WeightedGraph<T>(size);
+        }
     }
 }
